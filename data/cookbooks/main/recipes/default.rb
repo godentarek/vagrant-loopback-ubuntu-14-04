6288bf47319b7dbe636da8bdf38dea9e10869b52
Nickleus73 <n.moricet@free.fr>
2014-12-03 17:32:36 +0100

    Changed the default base to install loopback

--- data/cookbooks/main/recipes/default.rb
+++ data/cookbooks/main/recipes/default.rb
@@ -1,29 +1,44 @@
-execute "Installing generator-loopback..." do
-    command "rm -rf ~/.npm && npm cache clean"
+#execute "Cleaning cache npm..." do
+#    command "rm -rf ~/.npm && npm cache clean"
+#    user "root"
+#end
+
+#execute "Installing yo..." do
+#    command "npm install -g yo"
+#    user "root"
+#end
+
+#execute "Installing generator-loopback..." do
+#    command "npm install -g generator-loopback"
+#    user "root"
+#end
+
+#execute "Cleaning cache npm..." do
+#    command "rm -rf ~/.npm && npm cache clean"
+#    user "root"
+#end
+
+#execute "Installing strong-cli..." do
+#    command "npm install --unsafe-perm -g strong-cli"
+#    user "root"
+#end
+
+#execute "Cleaning cache npm..." do
+#    command "rm -rf ~/.npm && npm cache clean"
+#    user "root"
+#end
+
+#execute "Installing strong-cli..." do
+#    command "npm install -g strong-cli"
+#    user "root"
+#end
+
+execute "Installing strongloop..." do
+    command "npm install -g strongloop"
     user "root"
 end
 
-execute "Installing yo..." do
-    command "npm install -g yo"
-    user "root"
-end
-
-execute "Installing generator-loopback..." do
-    command "npm install -g generator-loopback"
-    user "root"
-end
-
-execute "Installing generator-loopback..." do
-    command "rm -rf ~/.npm && npm cache clean"
-    user "root"
-end
-
-execute "Installing strong-cli..." do
-    command "npm install -g strong-cli"
-    user "root"
-end
-
-execute "Installing generator-loopback..." do
+execute "Cleaning cache npm..." do
     command "rm -rf ~/.npm && npm cache clean"
     user "root"
 end
--- data/cookbooks/main/recipes/vagrant.rb
+++ data/cookbooks/main/recipes/vagrant.rb
@@ -3,15 +3,35 @@ execute "Installing software-properties-common python-software-properties python
     user "root"
 end
 
-execute "Adding repository for nodejs" do
-    command "add-apt-repository ppa:chris-lea/node.js -y && apt-get update"
+execute "Update apt-get" do
+    command "apt-get update"
     user "root"
 end
 
-execute "Installing nodejs" do
-    command "apt-get -y install nodejs"
+execute "Installing nodejs..." do
+    command "apt-get install -y nodejs"
     user "root"
 end
+
+execute "Installing npm..." do
+    command "apt-get install -y npm"
+    user "root"
+end
+
+exectue "Create a symbolic link" do 
+    command "ln -s /usr/bin/nodejs /usr/bin/node"
+    user "root"
+end
+
+#execute "Adding repository for nodejs" do
+#    command "add-apt-repository ppa:chris-lea/node.js -y && apt-get update"
+#    user "root"
+#end
+
+#execute "Installing nodejs" do
+#    command "apt-get -y install nodejs"
+#    user "root"
+#end
 
 execute "Update apt-get" do
     command "apt-get update"
